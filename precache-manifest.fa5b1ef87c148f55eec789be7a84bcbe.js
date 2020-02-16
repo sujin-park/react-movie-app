1e017dc7523d5bf7b0a098e69557bc1b6667298f
sujin-park <psujin831@gmail.com>
2020-02-16 18:44:28 +0900

    Updates

---
+++ precache-manifest.fa5b1ef87c148f55eec789be7a84bcbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba84113a1b3b6cc20659c68f7629dd4c",
+    "url": "/react-move-app/index.html"
+  },
+  {
+    "revision": "03fd1584e81508594bfe",
+    "url": "/react-move-app/static/css/main.18b28e54.chunk.css"
+  },
+  {
+    "revision": "66c93037a4acf130e246",
+    "url": "/react-move-app/static/js/2.08ccf181.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-move-app/static/js/2.08ccf181.chunk.js.LICENSE"
+  },
+  {
+    "revision": "03fd1584e81508594bfe",
+    "url": "/react-move-app/static/js/main.5e08474d.chunk.js"
+  },
+  {
+    "revision": "e8703e8f088a79f4ff86",
+    "url": "/react-move-app/static/js/runtime-main.3913ac85.js"
+  }
+]);
