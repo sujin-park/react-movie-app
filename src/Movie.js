3fe18dd603364009e52e0583f3439d6ff6c6dd61
sujin-park <psujin831@gmail.com>
2020-02-16 18:52:20 +0900

    [A] 5.0 Deploying to Github Pages

--- src/Movie.js
+++ src/Movie.js
@@ -10,7 +10,7 @@ const Movie = ({ id, year, title, summary, poster, genres }) => {
         <h3 className="movie__title">{title}</h3>
         <ul className="movie__genres">{genres.map((genre, idx) => <li key={idx} className="genre">{genre}</li>)}</ul>
         <h5 className="movie__year">{year}</h5>
-        <p className="movie__summary">{summary}</p>
+        <p className="movie__summary">{summary.slice(0, 180)}...</p>
       </div>
     </div>
   );
